8f05bb2629b066e0c34c708ef340d7f4c11f186b
nico57000 <falcettanicolas@gmail.com>
2018-12-10 10:39:34 +0100

    version id

--- delete.php
+++ delete.php
@@ -10,12 +10,12 @@
     <main>
       <header>
         <div class="Titre">
-          <p><a id="logo" href="acceuil.php"><span class="hp">Harry Potter</span><br>Magical Creatures</p>
+          <p><a id="logo" href="index.php"><span class="hp">Harry Potter</span><br>Magical Creatures</p>
         </div>
       </header>
       <nav>
       <ul class="Menu">
-        <li><a id="AboutUs" href="acceuil.php">Acceuil</a></li>
+        <li><a id="AboutUs" href="index.php">Acceuil</a></li>
         <li><a id="Table" href="harrypotter.php?#Tableau">Liste</a>
           <ul>
             <li><a id="sub" href="#">Ordre Croissant</a><li>
@@ -34,11 +34,11 @@
       </nav>
 <table id="Tableau">
         <tr>
+          <th class="table">ID
+          </th>
           <th class="table">Nom
           </th>
           <th class="table">Description
-          </th>
-          <th class="table">id
           </th>
           <th class="table">image
           </th>
@@ -49,11 +49,15 @@
 $rep_txt = "./txt";
 $rep_img = "./img";
 if (isset($_POST['supp'])){
-                if (file_exists($rep_txt."/".$_POST['supp'].".txt") AND file_exists($rep_img."/".$_POST['supp'].".jpg")){
-                  unlink ($rep_txt."/".$_POST['supp'].".txt");
-                  unlink ($rep_img."/".$_POST['supp'].".jpg");
-                    echo "votre créatures a bien été supprimé";
+                if (file_exists($rep_txt."/".$_POST['supp'].".txt")){
+                  $view = fopen($rep_txt."/".$_POST['supp'].".txt","a");
+                  // unlink ($rep_txt."/".$_POST['supp'].".txt");
+                  // unlink ($rep_img."/".$_POST['supp'].".jpg");
+                  $separe[3]=0;
+                  fputs($view,$separe[3]);
+                  echo "votre créatures a bien été supprimée";
                 }
+                fclose($view);
 }
 if ($dir_txt = opendir($rep_txt)) {
     
@@ -65,19 +69,22 @@ if ($dir_txt = opendir($rep_txt)) {
             $file = fopen($path, "r");
             while (!feof($file)) {
                 $line                = fgets($file);
-                $separe              = explode(" : ", $line);
-                $tableau[$separe[0]] = $separe[1];
+                $separe              = explode("*%", $line);
+                //$tableau[$separe[0]] = $separe[1];
             }
             fclose($file);
             echo "<tr>";
-            foreach ($tableau as $key => $value) {
-                echo "<td>" . $value . "</td>";
+           if ($separe[3] == 1){
+              for ($i=0; $i <3 ; $i++) { 
+                echo "<td>".$separe[$i]."</td>";
+              }
+
+              $chemin =$rep_img."/".$separe[0].".JPG";
+              echo "<td><img src='$chemin' width='100px'></td>";
+              echo "<form action='delete.php' method='post' name='delete'><td><button type='submit' name='supp' value='$separe[0]'>
+              <img src='del.png' alt='' height='42' width='42'></button></td></form>";
+              echo "</tr>";
             }
-            echo "<td><img src=" . $rep_img . '/' . $tableau["id"] . ".JPG width='100px' ></td>";
-            echo "<form action='delete.php' method='post' name='delete'><td><button type='submit' name='supp' value='$tableau[id]'>
-            <img src='del.png' alt='' height='42' width='42'></button></td></form>";
-            
-            echo "</tr>";
         }
     }
 }
