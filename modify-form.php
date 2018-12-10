66a041c03862206280137cbe56339dc436fc51a4
Fares Qedira <Fares@Faress-MacBook-Pro.local>
2018-12-10 22:41:41 +0100

    revue formulaire modif

--- modify-form.php
+++ modify-form.php
@@ -16,13 +16,13 @@
     <nav>
         <ul class="Menu">
             <li><a id="AboutUs" href="index.php">Acceuil</a></li>
-            <li><a id="Table" href="harrypotter.php?#Tableau">Creatures</a></li>
+            <li><a id="Table" href="harrypotter.php?#Tableau">Liste</a></li>
 
             <li><a id="Contribute">Gestion</a>
                 <ul>
                     <li><a id="sub" href="create.php">Créer</a><li>
-                    <li><a id="sub" href="modif.php#Tableau">Modifier</a><li>
-                    <li><a id="sub" href="delete.php#Tableau">Supprimer</a><li>
+                    <li><a id="sub" href="modif.php">Modifier</a><li>
+                    <li><a id="sub" href="delete.php">Supprimer</a><li>
                 </ul>
             </li>
         </ul>
@@ -37,16 +37,16 @@
             </th>
         </tr>
 <?php
+
 $id = $_GET['creature'];
 $rep_txt = "./txt";
 $rep_img = "./img";
 $chemin = $rep_img . "/" . $id . ".jpg";
-
-    $file = fopen($rep_txt . "/" . $id . ".txt", "r") or die("Erreur de l'ouverture du fichier texte");
+$file = fopen($rep_txt . "/" . $id . ".txt", "r") or die("Erreur de l'ouverture du fichier texte");
 
     while (($line = fgets($file)) != false) {
         $colonne = explode('*%', $line);
-
+    
         echo "<tr>";
         echo "<td class='Table'>" . $colonne[1] . "</td>";
         echo "<td class='Table'>" . $colonne[2] . "</td>";
@@ -54,12 +54,14 @@ $chemin = $rep_img . "/" . $id . ".jpg";
         echo "</tr>";
 
         echo "<tr>";
-        echo "<form name='modify-form' action='modif.php' id='modif-form'><td class='Table'><center><textarea name='nom' form='modif-form' rows='5' cols='15'>".$colonne[1]."</textarea><center></td>";
+        echo "<form name='modify-form' action='modify-form.php?creature=$_GET[creature]'id='modif-form'method=POST><td class='Table'><center><textarea name='nom' form='modif-form' rows='5' cols='15'>".$colonne[1]."</textarea><center></td>";
         echo "<td class='Table'><center><textarea name='description' form='modif-form' width='50px' rows='5' cols='100'>".$colonne[2]."</textarea><center></td>";
-        echo "<td class='Table'><center><input type='button' class='button' value='Enregistrer les Modifications'><center></form></td>";
+        echo "<td class='Table'><center><input type='submit' name='mod' class='button' value='Enregistrer les Modifications'><center></form></td>";
         echo "</tr>";
         
 }
+
+
 
 ?>
     </table>
